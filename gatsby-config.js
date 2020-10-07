6f03550975a316831200edbf3537abb45ea23574
sieroniekuggy <sieroniekuggy@gmail.com>
2020-10-08 05:45:56 +0800

    gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Silence Ronald | Full-Stack Developer`,
-    author: `Silence Ronald`,
-    role: `Full-Stack Developer`,
+    title: `Bart Zalewski | Front-End Developer`,
+    author: `Bart Zalewski`,
+    role: `Front-End Developer`,
   },
   plugins: [
     {
@@ -17,6 +17,13 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-scroll-reveal`,
     {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
@@ -28,8 +35,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Silence Ronald | Full-Stack Developer`,
-        short_name: `Silence Ronald`,
+        name: `Bart Zalewski | Front-End Developer`,
+        short_name: `Bart Zalewski`,
         start_url: `/`,
         background_color: `#02D463`,
         theme_color: `#02D463`,
---
+++ gatsby-node.js
@@ -0,0 +1,27 @@
+const path = require("path")
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/blog.js")
+  const res = await graphql(`
+    query {
+      allContentfulBlogPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.slug}`,
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
+}
